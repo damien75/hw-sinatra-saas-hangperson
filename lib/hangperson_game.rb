28ce7ef89ae8daeb15d3b3d00ef4e8ab14e9c79d
damien75 <damiengoblot@gmail.com>
2015-11-09 13:53:22 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,46 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses , :word_with_guesses , :check_win_or_lose
+  
   def initialize(word)
     @word = word
+    @word_with_guesses = '-' * word.length
+    @guesses = ''
+    @wrong_guesses = ''
+    @check_win_or_lose = :play
+  end
+  
+  def guess(letter)
+    raise ArgumentError, 'Input guess must not be empty or nil' unless !letter.nil? && letter.length > 0
+    letter.downcase!
+    raise ArgumentError, 'Input guess must be a letter' unless letter >= 'a' && letter <= 'z'
+    if !@word.include?(letter)
+      if @wrong_guesses.include?(letter)
+        return false
+      else
+        @wrong_guesses << letter
+        if @wrong_guesses.length >= 7
+          @check_win_or_lose = :lose
+        end
+        return true
+      end
+    else
+      if @guesses.include?(letter)
+        return false
+      else
+        @guesses << letter
+        l_index = @word.index(letter)
+        while !l_index.nil? do
+          @word_with_guesses[l_index] = letter
+          l_index = @word.index(letter , l_index + 1)
+        end
+        if @word == @word_with_guesses
+          @check_win_or_lose = :win
+        end
+        return true
+      end
+    end
   end
 
   def self.get_random_word
